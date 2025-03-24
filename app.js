39f5ce8bc51b2791ed20e1af6fbe86d73ff778fd
sujch~ <sujchand2@gmail.com>
2025-03-23 20:58:33 -0400

    Completed Module 3: Added homepage at / and implemented dynamic rendering for /travel

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = 3000;
 
-// Set up Handlebars as the view engine
 app.engine('hbs', exphbs.engine({
     extname: 'hbs',
     defaultLayout: 'main',
@@ -13,16 +12,13 @@ app.engine('hbs', exphbs.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 
-// Serve static files (CSS, images) from public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register routes
 const travelRoutes = require('./app_server/routes/travel');
 app.use('/travel', travelRoutes);
 
-// Temporary route for home page (optional for testing)
 app.get('/', (req, res) => {
-    res.send('Welcome to Travlr Getaways - Visit /travel for the main page');
+    res.redirect('/travel');
 });
 
 app.listen(PORT, () => {
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,23 @@
-const travelController = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways' });
+const fs = require('fs').promises;
+const path = require('path');
+
+const travelController = async (req, res) => {
+    try {
+        // Read the trips.json file
+        const tripsPath = path.join(__dirname, '../../data/trips.json');
+        const data = await fs.readFile(tripsPath, 'utf8');
+        const trips = JSON.parse(data);
+        console.log('Trips data:', trips); // Debugging
+
+        // Render the travel.hbs template with the trips data
+        res.render('travel', {
+            title: 'Travlr Getaways',
+            trips: trips
+        });
+    } catch (error) {
+        console.error('Error reading trips.json:', error);
+        res.status(500).send('Internal Server Error');
+    }
 };
 
-module.exports = {
-    travelController
-};
+module.exports = { travelController };
